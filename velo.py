72fe65f2701b271c8cca3b4ac497fb2d964db048
Romain <romain.dupuis@edu.itescia.fr>
2020-04-28 21:42:45 +0200

    Testé sur fichier csv de 20 éléments. csv.reader ok

--- velo.py
+++ velo.py
@@ -1,55 +1,79 @@
+#!/usr/bin/python
 # coding:utf-8
+
 import datetime
 import os
 import csv
+import shutil
 
 
 def my_date():
     date = datetime.datetime.now()
     date_clean = date.strftime("%d-%m-%Y")
-
     return date_clean
 
-
 def checkPrice(price):
-    priceInt = int(price)
+    # price sous la forme [1234]
+    S = price[1:-1]
+    try :
+        priceInt = int(S)
+    except :
+        print ("price format error : '{}'".format(price))
+        return None
+    
     if priceInt > 99 and priceInt < 8000:
         return priceInt
     else:
-        print("error data")
+        print("price format outbound")
+        return None
 
-
-def cleaner(fichier):
+def cleanerNew(fichier):
     with open(fichier, "r") as f:
         ma_liste_a_retourner=[]
-        for line in f.readlines():
-            linebis = line.split(',')
-            for i in linebis:
-                if str.__contains__(i, "[") == True:
-                    S = i[1:-1]
-                    if S.isdigit():
-                        schecked = checkPrice(S)
-                        if schecked != "error data" and schecked!="None":
-                            ma_liste_a_retourner.append(line)
+        reader = csv.reader(f)
 
-    print(ma_liste_a_retourner)
+        # On saute la première ligne
+        next(reader)
+
+        for line in reader:
+            # TODO : Chaque ligne doit contenir 9 colonnes. Sinon on ne peut pas. Traiter l'erreur
+            #print len(line)
+
+            # On récupère les 9 colonnes
+            # Source Url,Id,Date Publication Annonce,Date Expiration Annonce,Titre,Texte Central,Prix,Ville,Code Postal
+            url, id, publish_date, expiration_date, title, text, price, city, postal_code = line
+
+            # On traite chaque colonne pour s'assurer qu'elles sont correctes
+            checked_price = checkPrice(price)
+            if checked_price is not None:
+                print (checked_price)
+                ma_liste_a_retourner.append(str(line))
+
+            # TODO : Si erreur, ça pourrait etre bien de créer une liste pour cela. On pourrait la mettre ensuite dans un fichier
+
     return ma_liste_a_retourner
 
-                        
+input_file = "leboncoin.csv"
+output_dir = "source_" + my_date()
+file_prefix = "lbc_source_" + my_date()
+
+# Création répertoire de travail
 path = "source_" + my_date() + "/lbc_source_" + my_date()
+try :
+    os.mkdir(output_dir)
+    print("Create directory source_{}".format(output_dir))
+except:
+    print("Use existing directory source_{}".format(my_date()))
 
-os.mkdir('source_' + my_date())
+# Backup du fichier d'entrée dans le répertoire de travail (avant de commencer à jouer avec)
+shutil.copy2(input_file, "{}/{}_backup.csv".format(output_dir, file_prefix))
 
-with open('leboncoin.csv', 'r+') as f:
-    data = f.read()
-    # print(data)
+# Traitement de la colonne prix
+lbc_price = "{}/{}_lbc_price_ok.csv".format(output_dir, file_prefix)
 
-nom_du_fichier = path + ".csv"
-with open(nom_du_fichier, "w") as f:
-    f.write(data)
+with open(lbc_price, "w") as f :
+    cleaned_array = cleanerNew(input_file)
+    for elem in cleaned_array:
+        # On saute une ligne entre chaque insertion
+        f.write(elem + '\n')
     f.close()
-
-lbc_price = path + "lbc_price_ok.csv"
-with open(lbc_price, "w") as csv:
-    csv.write(str(cleaner(nom_du_fichier)))
-    csv.close()
